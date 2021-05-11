b2712dc88659b2ece35c4c94bce57f39d2b50c42
blenderskool <akash.hamirwasia@gmail.com>
2021-05-11 22:28:47 +0530

    Remove unused import

--- editor/src/components/ui/RadioTabs.vue
+++ editor/src/components/ui/RadioTabs.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref } from 'vue';
+import { defineComponent, PropType } from 'vue';
 
 export default defineComponent({
   name: 'Output',
