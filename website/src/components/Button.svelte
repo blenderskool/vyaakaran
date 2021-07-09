775b3756ffce9eb73d20ee21ee69af321c7b2ee1
blenderskool <akash.hamirwasia@gmail.com>
2021-07-10 00:18:29 +0530

    Add Button and LinkCta components

---
+++ website/src/components/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let variant: 'primary' | 'secondary' = 'primary';
+</script>
+
+{#if variant === 'primary'}
+  <button {...$$props} class={`bg-cyan-300 rounded text-blue-gray-900 px-5 py-2 font-semibold shadow-lg transition transform hover:(scale-105 bg-cyan-400) ${$$props.class}`}>
+    <slot />
+  </button>
+{:else if variant === 'secondary'}
+  <button {...$$props} class={`bg-cyan-300 bg-opacity-10 rounded text-cyan-300 px-5 py-2 font-medium shadow-lg transition transform hover:(scale-105 bg-opacity-20) disabled:(cursor-not-allowed opacity-75 scale-100) ${$$props.class}`}>
+    <slot />
+  </button>
+{/if}
---
+++ website/src/components/LinkCta.svelte
@@ -0,0 +1,6 @@
+<a {...$$props} class={`text-cyan-300 inline-block group ${$$props.class || ''}`}>
+  <slot />
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline ml-1 transform transition-transform group-hover:translate-x-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+  </svg>
+</a>
