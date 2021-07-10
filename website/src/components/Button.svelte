52e4cf29fdd6236398b9a0adeb6758df6d1965a4
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2021-07-10 19:15:43 +0530

    Update Button to also be anchor

--- website/src/components/Button.svelte
+++ website/src/components/Button.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
+  export let as: string = 'button';
   export let variant: 'primary' | 'secondary' = 'primary';
+
+  let classes = {
+    primary: `bg-cyan-300 rounded text-blue-gray-900 px-5 py-2 font-semibold shadow-lg text-shadow-none transition transform hover:(scale-105 bg-cyan-400) ${$$props.class}`,
+    secondary: `bg-cyan-300 bg-opacity-10 rounded text-cyan-300 px-5 py-2 font-medium shadow-lg text-shadow-none transition transform hover:(scale-105 bg-opacity-20) disabled:(cursor-not-allowed opacity-75 scale-100) ${$$props.class}`,
+  };
 </script>
 
-{#if variant === 'primary'}
-  <button {...$$props} class={`bg-cyan-300 rounded text-blue-gray-900 px-5 py-2 font-semibold shadow-lg transition transform hover:(scale-105 bg-cyan-400) ${$$props.class}`}>
+{#if as === 'button'}
+  <button {...$$props} class={classes[variant]}>
     <slot />
   </button>
-{:else if variant === 'secondary'}
-  <button {...$$props} class={`bg-cyan-300 bg-opacity-10 rounded text-cyan-300 px-5 py-2 font-medium shadow-lg transition transform hover:(scale-105 bg-opacity-20) disabled:(cursor-not-allowed opacity-75 scale-100) ${$$props.class}`}>
+{:else if as === 'a'}
+  <a {...$$props} class={classes[variant]}>
     <slot />
-  </button>
+  </a>
 {/if}
