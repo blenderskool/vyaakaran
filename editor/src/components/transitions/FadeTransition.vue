b8638580995485055c8fd7771a0f9e64d02f2e8b
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2021-08-31 01:17:38 +0530

    Update transition to receive all props

--- editor/src/components/transitions/FadeTransition.vue
+++ editor/src/components/transitions/FadeTransition.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition name="fade" enter-active-class="transition" leave-active-class="transition" enter-from-class="opacity-0" leave-to-class="opacity-0" appear>
+  <transition enter-active-class="transition" leave-active-class="transition" enter-from-class="opacity-0" leave-to-class="opacity-0" v-bind="$attrs">
     <slot />
   </transition>
 </template>
--- editor/src/components/transitions/GrowSlideTransition.vue
+++ editor/src/components/transitions/GrowSlideTransition.vue
@@ -1,12 +1,12 @@
 <template>
   <transition
-    appear
     enter-from-class="translate-y-4 scale-95"
     enter-to-class="translate-y-0 scale-100"
     enter-active-class="transition transform"
     leave-active-class="transition transform"
     leave-from-class="translate-y-0 scale-100"
     leave-to-class="translate-y-4 scale-95"
+    v-bind="$attrs"
   >
     <slot />
   </transition>
