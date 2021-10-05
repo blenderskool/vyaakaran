a1a8b9bece9804173f34bf03fdca4045c5dce41b
blenderskool <akash.hamirwasia@gmail.com>
2021-10-05 16:12:03 +0530

    Update hljs import to enable tree-shaking

--- editor/src/shims-vue.d.ts
+++ editor/src/shims-vue.d.ts
@@ -3,3 +3,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+declare module 'highlight.js/lib/core' {
+  import * as hljs from 'highlight.js';
+  export default hljs;
+}
