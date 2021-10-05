e9a8693f68c2957ab4d83f102046317329d54966
blenderskool <akash.hamirwasia@gmail.com>
2021-10-05 16:10:11 +0530

    Add rollup bundle visualizer

--- editor/vite.config.ts
+++ editor/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import WindiCSS from 'vite-plugin-windicss';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,9 @@ export default defineConfig({
     vue(),
     WindiCSS(),
   ],
+  build: {
+    rollupOptions: {
+      plugins: [visualizer({ gzipSize: true })],
+    }
+  }
 })
