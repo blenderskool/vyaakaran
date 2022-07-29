c227871d8abc61ab5865ac351181d800005a3e76
Tarun M <tarunsetty2002@gmail.com>
2022-07-29 16:47:31 +0530

    Adding State Transition Grammar Theme

--- editor/src/config/editor.ts
+++ editor/src/config/editor.ts
@@ -1,3 +1,4 @@
+import { root } from './../../node_modules/postcss/lib/postcss.d';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import 'monaco-editor/esm/vs/editor/contrib/find/findController';
 import 'monaco-editor/esm/vs/editor/contrib/multicursor/multicursor';
@@ -12,6 +13,21 @@ monaco.languages.setMonarchTokensProvider('Vyaakaran Grammar', {
       [/\/\/.*/, 'comment'],
       [/([A-Z][^\.#$ελ\/ \-|]*)/, 'non-terminal', ],
       [/([^A-Z\.#$ελ\/ \-|][^\.#$ελ\/ \-|]*)/, 'terminal', ],
+    ],
+  },
+});
+
+monaco.languages.register({ id: 'State Transition Grammar' });
+monaco.languages.setMonarchTokensProvider('State Transition Grammar', {
+  tokenizer: {
+    root: [
+      [/#/, 'keyword'],
+      [/[A-Z]\w./, 'state'],
+      [/[0-9a-z]/, 'symbol'],
+      [/-/, 'hyphen'],
+      [/[<>=]/, 'direction'],
+      [/[\(\):]/, 'seperator'],
+      [/\/\/.*/, 'comment'],
     ],
   },
 });
@@ -34,6 +50,26 @@ monaco.editor.defineTheme('vyaakaran', {
     'editor.selectionBackground': '#ffffff20',
   },
 });
+
+monaco.editor.defineTheme('statetransitiongrammar', {
+  base: 'vs-dark',
+  inherit: false,
+  rules: [
+    { token: 'keyword', foreground: '#0891B2', fontStyle: 'bold' },
+    { token: 'state', foreground: '#d9e9ff' },
+    { token: 'source', foreground: '#d6e9ff' },
+    { token: 'symbol', foreground: '#67E8F9' },
+    { token: 'hyphen', foreground: '#64748B' },
+    { token: 'direction', foreground: '#67E8F9' },
+    { token: 'seperator', foreground: '#64748B' },
+    { token: 'comment', foreground: '#475569'}
+  ],
+  colors: {
+    'editorLineNumber.foreground': '#444c55',
+    'editor.background': '#18181B',
+    'editor.selectionBackground': '#ffffff20',
+  },
+})
 
 const editorConfig: monaco.editor.IStandaloneEditorConstructionOptions = {
   theme: 'vyaakaran',
