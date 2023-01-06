4a2106dc2eea3bca8df39eeee6f4b19da812197d
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2023-01-06 19:09:49 +0000

    Move example selector menu to editor

--- editor/src/components/EditorTabs/EditorTabs.vue
+++ editor/src/components/EditorTabs/EditorTabs.vue
@@ -22,96 +22,6 @@
     </ul>
 
     <div class="flex justify-between items-center">
-      <Menu as="div" class="relative inline-block text-left mr-5">
-        <div>
-          <MenuButton class="inline-flex justify-center w-full rounded outline-none border text-xs border-cyan-300 hover:outline-none p-1 text-cyan-300 bg-cyan-400 bg-opacity-10 hover:bg-opacity-20 focus:outline-none">
-            Select Example
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-4 w-4"
-              aria-hidden="true"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </MenuButton>
-        </div>
-
-        <transition
-          enter-active-class="transition ease-out duration-100"
-          enter-from-class="transform opacity-0 scale-95"
-          enter-to-class="transform opacity-100 scale-100"
-          leave-active-class="transition ease-in duration-75"
-          leave-from-class="transform opacity-100 scale-100"
-          leave-to-class="transform opacity-0 scale-95"
-        >
-          <MenuItems class="origin-top-right absolute right-0 mt-2 w-56 rounded shadow-lg bg-black focus:outline-none">
-            <div class="py-1" v-if="store?.type === 'TM'">
-              <MenuItem
-                v-slot="{ active }"
-                v-for="item in dropdownDataTM"
-                :key="item.name"
-                @click="store.program = item.code"
-              >
-                <div
-                  :class="[
-                    active
-                      ? 'bg-cyan-400 text-cyan-300 bg-opacity-10'
-                      : 'text-cool-gray-400',
-                    'block px-4 py-2 text-xs cursor-pointer',
-                  ]"
-                >
-                  {{ item.name }}
-                </div>
-              </MenuItem>
-            </div>
-            <div class="py-1" v-else-if="store?.type === 'RG'">
-              <MenuItem
-                v-slot="{ active }"
-                v-for="item in dropdownDataRG"
-                :key="item.name"
-                @click="store.program = item.code"
-              >
-                <div
-                  :class="[
-                    active
-                      ? 'bg-cyan-400 text-cyan-300 bg-opacity-10'
-                      : 'text-cool-gray-400',
-                    'block px-4 py-2 text-xs cursor-pointer',
-                  ]"
-                >
-                  {{ item.name }}
-                </div>
-              </MenuItem>
-            </div>
-            <div class="py-1" v-else>
-              <MenuItem
-                v-slot="{ active }"
-                v-for="item in dropdownDataCFG"
-                :key="item.name"
-                @click="store.program = item.code"
-              >
-                <div
-                  :class="[
-                    active
-                      ? 'bg-cyan-400 text-cyan-300 bg-opacity-10'
-                      : 'text-cool-gray-400',
-                    'block px-4 py-2 text-xs cursor-pointer',
-                  ]"
-                >
-                  {{ item.name }}
-                </div>
-              </MenuItem>
-            </div>
-          </MenuItems>
-        </transition>
-      </Menu>
-
       <nav class="mx-2 flex-shrink-0 text-xs text-cyan-300 font-medium">
         <a href="https://vyaakaran.vercel.app/docs/syntax" target="_blank">
           Learn Syntax
@@ -125,15 +35,9 @@
 </template>
 
 <script lang="ts">
-import { computed, ComputedRef, defineComponent, inject, ref } from 'vue';
+import { computed, ComputedRef, defineComponent, inject } from 'vue';
 import { useRouter } from 'vue-router';
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
 import { playgrounds, Playground, getActivePlayground } from '../../store/code';
-import {
-  dropDownDataTM,
-  dropDownDataRG,
-  dropDownDataCFG,
-} from '../../utils/dropDownData';
 import useKeyShortcut from '../../utils/useKeyShortcut';
 import Tab from './Tab.vue';
 
@@ -141,10 +45,6 @@ export default defineComponent({
   name: 'EditorTabs',
   components: {
     Tab,
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuItems,
   },
   emits: ['new-playground'],
   setup() {
@@ -152,10 +52,6 @@ export default defineComponent({
     const tabs = computed(() => playgrounds.map(p => ({ name: p.name, lang: p.type })));
     const router = useRouter();
     const tabIdx = computed(() => Number(router.currentRoute.value.params.id) || 0);
-
-    const dropdownDataTM = ref(dropDownDataTM);
-    const dropdownDataRG = ref(dropDownDataRG);
-    const dropdownDataCFG = ref(dropDownDataCFG);
 
     const removeTab = (i: number) => {
       const mutate = () => {
@@ -198,9 +94,6 @@ export default defineComponent({
       tabIdx,
       removeTab,
       renameTab,
-      dropdownDataTM,
-      dropdownDataRG,
-      dropdownDataCFG,
     };
   },
 });
